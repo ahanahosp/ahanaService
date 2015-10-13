3ee3fa05b90dafe4688831c86f3f1335081bb0e4
Selvaraj K <ahanahosp@gmail.com>
2015-10-13 14:43:56 +0530

    Schema

--- src/main/resources/db/release-1/ahana.sql
+++ src/main/resources/db/release-1/ahana.sql
@@ -18,6 +18,31 @@ USE `ahana`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `floor`
+--
+
+DROP TABLE IF EXISTS `floor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `floor` (
+  `oid` varchar(20) NOT NULL,
+  `floor_name` varchar(100) NOT NULL,
+  `floor_code` varchar(20) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  PRIMARY KEY (`oid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `floor`
+--
+
+LOCK TABLES `floor` WRITE;
+/*!40000 ALTER TABLE `floor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `floor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `lookup`
 --
 
@@ -46,6 +71,64 @@ INSERT INTO `lookup` VALUES ('A+','BLOODGROUP','A+',1,0),('A-','BLOODGROUP','A-'
 UNLOCK TABLES;
 
 --
+-- Table structure for table `organization`
+--
+
+DROP TABLE IF EXISTS `organization`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `organization` (
+  `oid` varchar(20) NOT NULL,
+  `organization_name` varchar(100) NOT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `country` varchar(50) DEFAULT NULL,
+  `state` varchar(50) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `fax` varchar(15) DEFAULT NULL,
+  `email_id` varchar(100) NOT NULL,
+  `mobile_no` varchar(12) NOT NULL,
+  `primary_contact` varchar(12) DEFAULT NULL,
+  `secondary_contact` varchar(12) DEFAULT NULL,
+  PRIMARY KEY (`oid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `organization`
+--
+
+LOCK TABLES `organization` WRITE;
+/*!40000 ALTER TABLE `organization` DISABLE KEYS */;
+INSERT INTO `organization` VALUES ('a0100000003000000011','Ahana Hospital','Madurai','India','Tamil Nadu','Madurai','9898798978','ahana@gmail.com','98988787898','483784738473','384738748734');
+/*!40000 ALTER TABLE `organization` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `organization_module`
+--
+
+DROP TABLE IF EXISTS `organization_module`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `organization_module` (
+  `oid` varchar(20) NOT NULL,
+  `module_name` varchar(100) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  PRIMARY KEY (`oid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `organization_module`
+--
+
+LOCK TABLES `organization_module` WRITE;
+/*!40000 ALTER TABLE `organization_module` DISABLE KEYS */;
+INSERT INTO `organization_module` VALUES ('a0100000003000000005','Configuratiion','ACT'),('a0100000003000000006','Billing','ACT'),('a0100000003000000007','Patients','ACT'),('a0100000003000000008','Patient Registration','ACT');
+/*!40000 ALTER TABLE `organization_module` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `patient`
 --
 
@@ -54,38 +137,19 @@ DROP TABLE IF EXISTS `patient`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `patient` (
   `oid` varchar(20) NOT NULL,
-  `address` varchar(100) NOT NULL,
-  `blood_group` varchar(10) DEFAULT NULL,
-  `care_taker_name` varchar(100) DEFAULT NULL,
-  `category` varchar(50) DEFAULT NULL,
-  `city` varchar(50) NOT NULL,
-  `country` varchar(50) NOT NULL,
-  `dob` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `email_id` varchar(100) DEFAULT NULL,
   `first_name` varchar(50) NOT NULL,
-  `gender` varchar(6) NOT NULL,
   `last_name` varchar(50) DEFAULT NULL,
-  `father_or_spouse_name` varchar(100) DEFAULT NULL,
-  `marital_status` varchar(15) NOT NULL,
-  `mobile` varchar(12) DEFAULT NULL,
-  `occupation` varchar(100) DEFAULT NULL,
-  `patient_reference` varchar(15) DEFAULT NULL,
-  `patient_type` varchar(50) NOT NULL,
-  `permanent_address` varchar(100) DEFAULT NULL,
-  `permanent_city` varchar(50) DEFAULT NULL,
-  `permanent_country` varchar(50) DEFAULT NULL,
-  `permanent_state` varchar(50) DEFAULT NULL,
-  `permanent_zip` varchar(10) DEFAULT NULL,
-  `primary_contact` varchar(12) DEFAULT NULL,
-  `referal_doctor` varchar(100) DEFAULT NULL,
-  `referal_hospital` varchar(100) DEFAULT NULL,
-  `registration_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `registration_mode` varchar(10) DEFAULT NULL,
-  `salutation` varchar(10) NOT NULL,
-  `secondary_contact` varchar(12) DEFAULT NULL,
-  `state` varchar(50) NOT NULL,
-  `zip` varchar(10) NOT NULL,
-  `care_taker_type` varchar(50) DEFAULT NULL,
+  `gender` varchar(6) NOT NULL,
+  `mobile` varchar(12) NOT NULL,
+  `age` int(11) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `state` varchar(50) DEFAULT NULL,
+  `country` varchar(50) DEFAULT NULL,
+  `zip` varchar(10) DEFAULT NULL,
+  `category` varchar(50) DEFAULT NULL,
+  `billing` varchar(50) DEFAULT NULL,
+  `registration_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`oid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -96,7 +160,33 @@ CREATE TABLE `patient` (
 
 LOCK TABLES `patient` WRITE;
 /*!40000 ALTER TABLE `patient` DISABLE KEYS */;
+INSERT INTO `patient` VALUES ('PAT10','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT11','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT12','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT13','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT14','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT15','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT16','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT17','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT18','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT19','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT20','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT21','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT22','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT23','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT24','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT25','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT26','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT27','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT28','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT29','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT3','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT30','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT31','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT32','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT33','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT34','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT35','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT36','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT37','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT38','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT39','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT4','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT40','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT41','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT42','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT43','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT44','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT45','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT46','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT47','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT48','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT49','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT5','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT50','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT51','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT52','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT53','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT54','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT55','TestAbc','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT56','TestAbc','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT57','TestAbc','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT58','TestAbc','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT59','TestAbc','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT6','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT60','TestAbc','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT61','TestAbc','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT62','TestAbc','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT63','TestAbc','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT64','TestAbc','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT65','TestAbc','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT7','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT8','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00'),('PAT9','test','test','Male','9898989899',53,'tes123232t','Madurai','TamilNadu','india','78787878','Standard','standard','2015-10-06 18:30:00');
 /*!40000 ALTER TABLE `patient` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role_rights`
+--
+
+DROP TABLE IF EXISTS `role_rights`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `role_rights` (
+  `oid` varchar(20) NOT NULL,
+  `role_oid` varchar(20) NOT NULL,
+  `module_oid` varchar(20) NOT NULL,
+  `organization_oid` varchar(20) NOT NULL,
+  PRIMARY KEY (`oid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role_rights`
+--
+
+LOCK TABLES `role_rights` WRITE;
+/*!40000 ALTER TABLE `role_rights` DISABLE KEYS */;
+/*!40000 ALTER TABLE `role_rights` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -120,7 +210,35 @@ CREATE TABLE `roles` (
 
 LOCK TABLES `roles` WRITE;
 /*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES ('a0100000004000000006','Admin','ACT'),('a0100000004000000007','User','ACT'),('a0100000004000000008','Super_Admin','ACT');
 /*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `room`
+--
+
+DROP TABLE IF EXISTS `room`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `room` (
+  `oid` varchar(20) NOT NULL,
+  `bed_name` varchar(100) NOT NULL,
+  `ward_oid` varchar(20) NOT NULL,
+  `occupancy_status` varchar(20) NOT NULL DEFAULT 'Vaccunt',
+  `maintenance_status` varchar(20) NOT NULL DEFAULT 'Not Available',
+  `status` varchar(20) NOT NULL,
+  PRIMARY KEY (`oid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `room`
+--
+
+LOCK TABLES `room` WRITE;
+/*!40000 ALTER TABLE `room` DISABLE KEYS */;
+/*!40000 ALTER TABLE `room` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -146,7 +264,7 @@ CREATE TABLE `seed_container` (
 
 LOCK TABLES `seed_container` WRITE;
 /*!40000 ALTER TABLE `seed_container` DISABLE KEYS */;
-INSERT INTO `seed_container` VALUES (0,'a01',1,'REST'),(1,'PAT',2,'PAT');
+INSERT INTO `seed_container` VALUES (5,'a01',1,'REST'),(3,'PAT',2,'PAT');
 /*!40000 ALTER TABLE `seed_container` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -159,18 +277,26 @@ DROP TABLE IF EXISTS `user_profile`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_profile` (
   `oid` varchar(20) NOT NULL,
-  `first_name` varchar(30) NOT NULL,
-  `last_name` varchar(30) NOT NULL,
-  `last_logged_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `salutation` varchar(10) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
   `password` varchar(100) NOT NULL,
   `password_exp_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `user_id` varchar(100) NOT NULL,
   `user_status` varchar(5) NOT NULL,
-  `user_type` varchar(20) DEFAULT NULL,
-  `account_expired` tinyint(1) NOT NULL DEFAULT '0',
-  `account_locked` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`oid`),
-  UNIQUE KEY `user_profile_user_id_key` (`user_id`)
+  `address` varchar(100) DEFAULT NULL,
+  `country` varchar(50) DEFAULT NULL,
+  `state` varchar(50) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `zip` varchar(10) DEFAULT NULL,
+  `email_id` varchar(100) NOT NULL,
+  `mobile_no` varchar(12) NOT NULL,
+  `primary_contact` varchar(12) DEFAULT NULL,
+  `secondary_contact` varchar(12) DEFAULT NULL,
+  `designation` varchar(100) DEFAULT NULL,
+  `speciality` varchar(100) DEFAULT NULL,
+  `care_provider` varchar(100) DEFAULT 'FALSE',
+  PRIMARY KEY (`oid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -180,6 +306,7 @@ CREATE TABLE `user_profile` (
 
 LOCK TABLES `user_profile` WRITE;
 /*!40000 ALTER TABLE `user_profile` DISABLE KEYS */;
+INSERT INTO `user_profile` VALUES ('a0100000003000000005','Mr','Selva','Raj','e08a7c49d96c2b475656cc8fe18cee8e','2015-09-21 11:04:50','selva','ACT',NULL,NULL,NULL,NULL,NULL,'selva@gmail.com','9791034869',NULL,NULL,NULL,NULL,'FALSE');
 /*!40000 ALTER TABLE `user_profile` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -207,6 +334,31 @@ LOCK TABLES `user_roles` WRITE;
 /*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
 /*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `ward`
+--
+
+DROP TABLE IF EXISTS `ward`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ward` (
+  `oid` varchar(20) NOT NULL,
+  `ward_name` varchar(100) NOT NULL,
+  `floor_oid` varchar(20) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  PRIMARY KEY (`oid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ward`
+--
+
+LOCK TABLES `ward` WRITE;
+/*!40000 ALTER TABLE `ward` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ward` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -217,4 +369,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-10-08 23:15:15
+-- Dump completed on 2015-10-13 14:35:44
