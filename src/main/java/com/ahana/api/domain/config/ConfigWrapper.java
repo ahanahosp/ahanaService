a8d3eecb8030cb8b5dc40e698e3b067801a2c70b
Selvaraj K <ahanahosp@gmail.com>
2015-11-30 14:58:25 +0530

    Fix

--- src/main/java/com/ahana/api/domain/config/ConfigWrapper.java
+++ src/main/java/com/ahana/api/domain/config/ConfigWrapper.java
@@ -3,12 +3,15 @@ package com.ahana.api.domain.config;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import com.ahana.commons.system.domain.user.Roles;
 
 public class ConfigWrapper implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Valid
 	private List<Roles> roles;
 
 	public List<Roles> getRoles() {
